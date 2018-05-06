94da34669edd844f723f027951b8b9b9059a22b7
ymishin <yury.mishin@gmail.com>
2018-05-06 18:22:50 +0200

    Vaadin 10 Project Base with Spring.

---
+++ src/main/java/com/example/test/spring/ExampleServletInitializer.java
@@ -0,0 +1,24 @@
+package com.example.test.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * The entry point of the Spring Boot application.
+ */
+@SpringBootApplication
+public class ExampleServletInitializer extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleServletInitializer.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(ExampleServletInitializer.class);
+    }
+
+}
---
+++ src/main/java/com/example/test/spring/MainView.java
@@ -0,0 +1,26 @@
+package com.example.test.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+/**
+ * The main view contains a simple label element and a template element.
+ */
+@HtmlImport("styles/shared-styles.html")
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView(@Autowired ExampleTemplate template) {
+        // This is just a simple label created via Elements API
+        Button button = new Button("Click me",
+                event -> template.setValue("Clicked!"));
+        // This is a simple template example
+        add(button, template);
+        setClassName("main-layout");
+    }
+
+}
